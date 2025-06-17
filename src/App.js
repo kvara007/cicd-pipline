a7c936b053c08eeb92ea4070b2eb57209228f567
Zviad Kvaratskhelia <zviad.kvara000@gmail.com>
2025-06-17 20:32:17 +0400

    Modify App.js file

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome</h1>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Edited and now it's main branch...
+          Edited <code>src/App.js</code> and now it's dev branch.
         </p>
       </div>
     );
