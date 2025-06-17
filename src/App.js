5ea2a9ea8677432b3a26f3a209595315480535e0
Zviad Kvaratskhelia <zviad.kvara000@gmail.com>
2025-06-17 20:31:13 +0400

    Modify app.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Edited <code>src/App.js</code> and now it's dev branch.
         </p>
       </div>
     );
